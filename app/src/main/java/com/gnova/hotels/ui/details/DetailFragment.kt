7b43801bddc64440a59159282b95c3f913292c6a
Goran Novakovic <gnovakov@gmail.com>
2021-11-10 23:02:42 +0000

    updated a discrepancy in Kotlin version between the app and the other modules

--- app/src/main/java/com/gnova/hotels/ui/details/DetailFragment.kt
+++ app/src/main/java/com/gnova/hotels/ui/details/DetailFragment.kt
@@ -1,6 +1,7 @@
 package com.gnova.hotels.ui.details
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -33,7 +34,7 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
     }
 
     private fun populateHotelInfo(hotel: HotelAvailability) {
-
+        Log.d("TAG", "HOTEL: ${hotel.hotelInfo.name}")
     }
 
 
